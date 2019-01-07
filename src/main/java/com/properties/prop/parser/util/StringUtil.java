971112a3425ec09ec0dd9aa3d3650a4da06ef72f
Mihai Popovici <misuvd92@yahoo.com>
2019-01-07 09:27:17 +0200

    Improved position similarity sort

--- src/main/java/com/properties/prop/parser/util/StringUtil.java
+++ src/main/java/com/properties/prop/parser/util/StringUtil.java
@@ -36,14 +36,20 @@ public class StringUtil {
     }
     public static double positionSimilarity(String x,String y){
         if(x.equals(y)) {
-            return 5d;
+            return 7d;
         }else  if(x.equalsIgnoreCase(y)){
-            return 4d;
-        }else if(x.contains(y)){
+            return 6d;
+        }else if(containsExact(x,y)){
+            double posYinX= getPositionExactIgnoreCase(x,y);
+            return 5d + (posYinX!=0&&posYinX!=-1 ? (1d/x.indexOf(y)) : 1d);
+        }else if(containsExactIgnoreCase(x,y)){
             int posYinX=x.indexOf(y);
+            return 4d + (posYinX!=0&&posYinX!=-1 ? (1d/x.indexOf(y)) : 1d);
+        }else if(StringUtils.contains(x,y)){
+            double posYinX=getPositionContains(x,y);
             return 3d + (posYinX!=0&&posYinX!=-1 ? (1d/x.indexOf(y)) : 1d);
         }else if(StringUtils.containsIgnoreCase(x,y)){
-            int posYinX=x.indexOf(y);
+            double posYinX=getPositionContainsIgnoreCase(x,y);
             return 2d + (posYinX!=0&&posYinX!=-1 ? (1d/x.indexOf(y)) : 1d);
         }else if(numberOfContainedStrings(x,y)!=0){
             double numberOfContainedStrings=numberOfContainedStrings(x,y);
@@ -53,8 +59,74 @@ public class StringUtil {
             return numberOfContainedStringsIgnoreCase != 0d ? 1d-1d/numberOfContainedStringsIgnoreCase : 0d;
         }
     }
+    private static boolean containsExact(String x,String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        boolean contains=false;
+        for(String token : tokens){
+            if(token.equals(y)) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
+    private static double getPositionExact(String x, String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        double position=-1;
+        for(int i=0;i<tokens.length;i++){
+            if(tokens[i].equals(y)) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+    private static double getPositionExactIgnoreCase(String x, String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        double position=-1;
+        for(int i=0;i<tokens.length;i++){
+            if(tokens[i].equalsIgnoreCase(y)) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+    private static double getPositionContains(String x, String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        double position=-1;
+        for(int i=0;i<tokens.length;i++){
+            if(tokens[i].contains(y)) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+    private static double getPositionContainsIgnoreCase(String x, String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        double position=-1;
+        for(int i=0;i<tokens.length;i++){
+            if(StringUtil.containsExactIgnoreCase(tokens[i],y)) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+    private static boolean containsExactIgnoreCase(String x,String y){
+        String[] tokens=x.split("(\\s+)|(,+)|(\\.+)|(:+)");
+        boolean contains=false;
+        for(String token : tokens){
+            if(token.equalsIgnoreCase(y)) {
+                contains = true;
+                break;
+            }
+        }
+        return contains;
+    }
     private static double numberOfContainedStrings(String x,String y){
-        String[] tokens=y.split("(\\\\s+)|(,+)|(\\.+)|(:+)");
+        String[] tokens=y.split("(\\s+)|(,+)|(\\.+)|(:+)");
         double number=0d;
         for(String token : tokens){
             if(x.contains(token)){
