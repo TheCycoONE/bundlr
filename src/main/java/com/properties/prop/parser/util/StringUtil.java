5388ea1a4acb6e34935fc586b88bb056ada9c3e1
popovicim <misuvd92@yahoo.com>
2019-01-09 11:10:17 +0200

    Improved similarity function.

--- src/main/java/com/properties/prop/parser/util/StringUtil.java
+++ src/main/java/com/properties/prop/parser/util/StringUtil.java
@@ -2,6 +2,8 @@ package com.properties.prop.parser.util;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.Arrays;
+
 public class StringUtil {
 
     public static final String SPECIAL_CHAR_REGEX = "( +)|(,+)|(\\.+)|(:+)|(;+)|('+)|(\"+)";
@@ -22,45 +24,47 @@ public class StringUtil {
     }
     public static double positionSimilarity(String x,String y){
         if(x.equals(y)) {
-            return 13d;
+            return 14d;
         }else  if(x.equalsIgnoreCase(y)){
-            return 12d;
+            return 13d;
         }else if(containsExact(x,y,false)){
             double posYinX=getPositionExact(x,y,false);
-            return 11d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 12d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(containsExactIgnoreCase(x,y,false)){
             double posYinX=getPositionExactIgnoreCase(x, y,false);
-            return 10d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 11d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(StringUtils.contains(x,y)){
             double posYinX=getPositionContains(x,y,false);
-            return 9d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 10d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(StringUtils.containsIgnoreCase(x,y)){
             double posYinX=getPositionContainsIgnoreCase(x,y,false);
-            return 8d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 9d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(containsExact(x,y,true)){
             double posYinX=getPositionExact(x,y,true);
-            return 7d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 8d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(containsExactIgnoreCase(x,y,true)){
             double posYinX=getPositionExactIgnoreCase(x, y,true);
-            return 6d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 7d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(StringUtils.contains(x,y)){
             double posYinX=getPositionContains(x,y,true);
-            return 5d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 6d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(StringUtils.containsIgnoreCase(x,y)){
             double posYinX=getPositionContainsIgnoreCase(x,y,true);
-            return 4d + (posYinX!=-1 ? (1d/posYinX) : 0d);
+            return 5d + (posYinX!=-1 ? (1d/posYinX) : 0d);
         }else if(numberOfExactStrings(x,y)!=0){
             double numberOfContainedStrings=numberOfContainedStrings(x,y);
-            return 3d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
+            return 4d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
         }else if(numberOfExactStringsIgnoreCase(x,y)!=0){
             double numberOfContainedStrings=numberOfContainedStrings(x,y);
-            return 2d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
+            return 3d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
         }else if(numberOfContainedStrings(x,y)!=0){
             double numberOfContainedStrings=numberOfContainedStrings(x,y);
-            return 1d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
-        }else{
+            return 2d + numberOfContainedStrings != 1d ? (1d-1d/numberOfContainedStrings) : 0d;
+        }else if(numberOfContainedStringsIgnoreCase(x,y)!=0){
             double numberOfContainedStringsIgnoreCase=numberOfContainedStringsIgnoreCase(x,y);
-            return numberOfContainedStringsIgnoreCase != 1d ? (1d-1d/numberOfContainedStringsIgnoreCase) : 0d;
+            return 1d+numberOfContainedStringsIgnoreCase != 1d ? (1d-1d/numberOfContainedStringsIgnoreCase) : 0d;
+        }else {
+            return (1d-(1d/minLevenshtein(x,y)));
         }
     }
     private static boolean containsExact(String x,String y,boolean escapeY){
@@ -77,6 +81,56 @@ public class StringUtil {
         }
         return contains;
     }
+
+    private static double minLevenshtein(String x,String y){
+        String[] tokens=x.split(SPECIAL_CHAR_REGEX);
+        double minLevenshtein=Double.MAX_VALUE;
+        double position=1;
+        double minPosition=2;
+        for(String token : tokens){
+            position++;
+            double levenshtein=levensthein(token,y);
+            if(levenshtein+position<minLevenshtein) {
+                minPosition=position;
+                minLevenshtein=levenshtein+position;
+            }
+        }
+        return minLevenshtein;
+    }
+
+    private static int levensthein(String x, String y) {
+        int[][] dp = new int[x.length() + 1][y.length() + 1];
+
+        for (int i = 0; i <= x.length(); i++) {
+            for (int j = 0; j <= y.length(); j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                }
+                else if (j == 0) {
+                    dp[i][j] = i;
+                }
+                else {
+                    dp[i][j] = min(dp[i - 1][j - 1]
+                                    + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
+                            dp[i - 1][j] + 1,
+                            dp[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+    private static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    private static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+
+
     private static boolean containsExactIgnoreCase(String x,String y,boolean escapeY){
         String[] tokens=x.split(SPECIAL_CHAR_REGEX);
         if(escapeY){
@@ -91,6 +145,7 @@ public class StringUtil {
         }
         return contains;
     }
+
     private static double getPositionExact(String x, String y,boolean escapeY){
         String[] tokens=x.split(SPECIAL_CHAR_REGEX);
         if(escapeY){
